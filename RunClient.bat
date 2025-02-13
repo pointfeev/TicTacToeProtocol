e952151f099168d8c93a0fc9fb431be035961fab
pointfeev <zehmeen@gmail.com>
2025-02-13 00:34:22 -0500

    Implement compile & run scripts

--- RunClient.bat
+++ RunClient.bat
@@ -1,4 +1,1 @@
-@echo off
-javac TicTacToeClient.java || (pause >nul && exit)
-java TicTacToeClient || (pause >nul && exit)
-del /q *.class >nul 2>nul
+@echo off && javac Client.java -d out && cd out && java Client %* || pause >nul
--- RunServer.bat
+++ RunServer.bat
@@ -1,4 +1,1 @@
-@echo off
-javac TicTacToeServer.java || (pause >nul && exit)
-java TicTacToeServer || (pause >nul && exit)
-del /q *.class >nul 2>nul
+@echo off && javac Server.java -d out && cd out && java Server %* || pause >nul
